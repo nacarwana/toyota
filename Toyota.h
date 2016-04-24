34910d72651433f7c993f3f7f032aebb914ee660
nacarwana <nac_carwana@hotmail.com>
2016-04-24 17:49:23 -0400

    Added files via upload

---
+++ Toyota.h
@@ -0,0 +1,13 @@
+//
+//  Toyota.h
+//  toyota
+//
+//  Created by Nathan Carwana on 2016-04-08.
+//  Copyright © 2016 Carwana Design. All rights reserved.
+//
+
+#import "Car.h"
+
+@interface Toyota : Car
+
+@end
---
+++ Toyota.m
@@ -0,0 +1,20 @@
+//
+//  Toyota.m
+//  toyota
+//
+//  Created by Nathan Carwana on 2016-04-08.
+//  Copyright © 2016 Carwana Design. All rights reserved.
+//
+
+#import "Toyota.h"
+
+@implementation Toyota
+
+-(id) init {
+        
+    self.model = @"Prius";
+    
+    return self;
+}
+
+@end
